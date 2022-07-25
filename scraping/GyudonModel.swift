93233ce12ab07063e096ee8e678ab65cd317dacb
kazukiomori <kazuki1110170501@gmail.com>
2022-07-25 23:31:34 +0900

    Modelを作成

---
+++ scraping/GyudonModel.swift
@@ -0,0 +1,13 @@
+//
+//  GyudonModel.swift
+//  scraping
+//
+//  Created by Kazuki Omori on 2022/07/25.
+//
+
+import Foundation
+
+class Gyudon: NSObject {
+    var size: String = ""
+    var price: String = ""
+}
